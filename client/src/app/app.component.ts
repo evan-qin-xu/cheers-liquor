4ab7dd9fefc9d9aa2bffe342171e957295d2e8a5
Evan Xu <evan.qin.xu@gmail.com>
2021-09-13 12:35:45 +1000

    Display products data from the fake store API.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {IPagination} from './models/pagination'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cheers Liquor';
+  
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://fakestoreapi.com/products/').subscribe((response: IPagination) => {
+      this.products = response.data;
+      console.log(this.products);
+    }, error => {
+      console.log(error);
+    });
+  }
+  
 }
+
